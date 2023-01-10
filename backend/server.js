644e7521f976e9de6a6fa0ec2912b26222987496
TheBarsoom <117264166+TheBarsoom@users.noreply.github.com>
2023-01-10 22:21:39 +0500

    S-1 ADD and Configure Server.js

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const dotEnv = require("dotenv");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const dbConnect = require("./config/dbConnect");
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+dotEnv.config({
+  path: "backend/config/config.env",
+});
+
+//use middleware-------------
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(cors({origin: "http://localhost:5137",credentials: true,})
+);
+// router use..............
+
+// db connect........
+dbConnect();
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, (error) => {
+  if (!error) {
+    console.log(`server is running port ${PORT}`);
+  }
+});
