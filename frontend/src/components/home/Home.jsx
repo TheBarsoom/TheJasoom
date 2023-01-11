63a4703bb607e55051377241a216f11e4997f79b
TheBarsoom <117264166+TheBarsoom@users.noreply.github.com>
2023-01-11 12:15:27 +0500

    F-4 Add Category & Tag Component in Home

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import Navbar from "./Navbar";
-
+import PopularArtical from "./PopularArtical";
+import { FaChevronRight } from "react-icons/fa";
 const Home = () => {
   return (
     <div className="home">
@@ -18,22 +19,45 @@ const Home = () => {
                   <h2>Search</h2>
                   <div className="form-group">
                     <input
-                      
                       className="form-control"
                       type="text"
                       placeholder="search"
                     />
                   </div>
                   <div className="form-group">
-                    <button className="btn btn-block">
-                      Search
-                    </button>
+                    <button className="btn btn-block">Search</button>
                   </div>
                 </div>
                 <div className="popular-artical">
                   <div className="title">
                     <h3>Popular Artical</h3>
                   </div>
+                  <PopularArtical />
+                </div>
+                <div className="category">
+                  <div className="title">
+                    <h3>Category</h3>
+                  </div>
+                  <ul className="cate-list">
+                    <div className="cate-item">
+                      <li>
+                        <FaChevronRight />
+                        <Link to="/">Link</Link>
+                      </li>
+                      <span>1</span>
+                    </div>
+                  </ul>
+                </div>
+
+                <div className="tag">
+                  <div className="title">
+                    <h3>Tag</h3>
+                  </div>
+                  <ul>
+                    <li>
+                      <Link to={`/artical/tag/`}>Tag Link</Link>
+                    </li>
+                  </ul>
                 </div>
               </div>
             </div>
--- frontend/src/components/home/PopularArtical.jsx
+++ frontend/src/components/home/PopularArtical.jsx
@@ -1,12 +1,33 @@
-
-import React from 'react'
-
+import React from 'react';
+import { Link } from "react-router-dom";
 const PopularArtical = () => {
-  return (
-    <div>
-      PopularArtical
-    </div>
-  )
-}
+  const data = [
+    {link:"/",img:"https://s1.1zoom.me/big3/207/373785-svetik.jpg",title:"Barsoom is wonderful",date:"August 9 2023"},
+    {link:"/",img:"https://s1.1zoom.me/big3/207/373785-svetik.jpg",title:"Barsoom is wonderful",date:"August 9 2023"},
+    {link:"/",img:"https://s1.1zoom.me/big3/207/373785-svetik.jpg",title:"Barsoom is wonderful",date:"August 9 2023"},
+    {link:"/",img:"https://s1.1zoom.me/big3/207/373785-svetik.jpg",title:"Barsoom is wonderful",date:"August 9 2023"},
+    {link:"/",img:"https://s1.1zoom.me/big3/207/373785-svetik.jpg",title:"Barsoom is wonderful",date:"August 9 2023"},
+    {link:"/",img:"https://s1.1zoom.me/big3/207/373785-svetik.jpg",title:"Barsoom is wonderful",date:"August 9 2023"}
+]
+    return (
+        <>
+           {data.map((e,i)=>(
+             <div className="row" key={i}>
+             <div className="col-4">
+                 <Link to={e.link} className='image'><img src={e.img} alt="" /></Link>
+             </div>
+             <div className="col-8">
+                 <div className="title-time">
+                     <Link to={e.link} >{e.title} </Link>
+                     <br />
+                     <span>{e.date}</span>
+                 </div>
+             </div>
+         </div>
+           ))}
+            
+        </>
+    )
+};
 
-export default PopularArtical
+export default PopularArtical;
